a895f5ad35945acc94a322fc846f041e859e44bb
Tung Nguyen <xtungit@gmail.com>
2020-08-18 09:43:51 +0700

    Initial Commit

---
+++ SPOS Merchant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SPOS Merchant
+//
+//  Created by Tung Nguyen on 8/18/20.
+//  Copyright © 2020 Tung Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
